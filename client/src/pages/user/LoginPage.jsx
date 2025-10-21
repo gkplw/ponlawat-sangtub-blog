bf3150683b492c39c3495676d595c425cf5ec5a6
Ponlawat <p.sangtub@gmail.com>
2025-10-21 09:57:21 +0700

    refactor: adjust code spacing in LoginPage

--- client/src/pages/user/LoginPage.jsx
+++ client/src/pages/user/LoginPage.jsx
@@ -165,6 +165,7 @@ export function LoginPage() {
           </div>
         </div>
       </main>
+      
       {/* Admin Link */}
       <div className="flex justify-center flex-16">
         <Link to="/admin/login" className="text-blue-600 underline hover:text-blue-300">
